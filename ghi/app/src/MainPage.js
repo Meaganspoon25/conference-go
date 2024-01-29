747f4d89a62d7376482b989a61303af6377702af
Meagan <weatherspoon.meagan@gmail.com>
2024-01-29 17:36:11 -0600

    Production Update Feature

--- ghi/app/src/MainPage.js
+++ ghi/app/src/MainPage.js
@@ -1,58 +1,82 @@
-import React, { useState, useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
-
-function Conference(props) {
-  const { conference } = props;
-
+function ConferenceColumn(props) {
   return (
-    <div className="g-col-3">
-        <div key="{conference.href}" className="card mb-3 shadow">
-          <img src={conference.location.picture_url} className="card-img-top" alt={conference.location.picture_url}/>
-
-          <div className="card-body">
-            <h5 className="card-title">{conference.name}</h5>
-            <h6 className="card-subtitle mb-2 text-muted">
-              {conference.location.name}
-            </h6>
-            <p className="card-text">
-              {conference.description}
-            </p>
+    <div className="col">
+      {props.list.map(data => {
+        const conference = data.conference;
+        return (
+          <div key={conference.href} className="card mb-3 shadow">
+            <img src={conference.location.picture_url} className="card-img-top" />
+            <div className="card-body">
+              <h5 className="card-title">{conference.name}</h5>
+              <h6 className="card-subtitle mb-2 text-muted">
+                {conference.location.name}
+              </h6>
+              <p className="card-text">
+                {conference.description}
+              </p>
+            </div>
+            <div className="card-footer">
+              {new Date(conference.starts).toLocaleDateString()}
+              -
+              {new Date(conference.ends).toLocaleDateString()}
+            </div>
           </div>
-
-          <div className="card-footer">
-            {new Date(conference.starts).toLocaleDateString()}
-            -
-            {new Date(conference.ends).toLocaleDateString()}
-          </div>
-        </div>
+        );
+      })}
     </div>
   );
 }
-
-function MainPage() {
-  const [conferences, setConferences] = useState([]);
-
-  async function getConferences() {
+const MainPage = (props) =>  {
+  const [conferenceColumns, setConferenceColumns] = useState([[], [], []]);
+  const fetchData = async () => {
     const url = 'http://localhost:8000/api/conferences/';
-    const response = await fetch(url);
-
-    if (response.ok) {
-      const data = await response.json();
-
-      const detailResponses = data.conferences.map(async conference => {
-        const response = await fetch(`http://localhost:8000/${conference.href}`);
-        return await response.json();
-      })
-
-      const conferenceDetails = await Promise.all(detailResponses)
-      setConferences(conferenceDetails);
+    try {
+      const response = await fetch(url);
+      if (response.ok) {
+        // Get the list of conferences
+        const data = await response.json();
+        // Create a list of for all the requests and
+        // add all of the requests to it
+        const requests = [];
+        for (let conference of data.conferences) {
+          const detailUrl = `http://localhost:8000${conference.href}`;
+          requests.push(fetch(detailUrl));
+        }
+        // Wait for all of the requests to finish
+        // simultaneously
+        const responses = await Promise.all(requests);
+        // Set up the "columns" to put the conference
+        // information into
+        const columns = [[], [], []];
+        // Loop over the conference detail responses and add
+        // each to to the proper "column" if the response is
+        // ok
+        let i = 0;
+        for (const conferenceResponse of responses) {
+          if (conferenceResponse.ok) {
+            const details = await conferenceResponse.json();
+            columns[i].push(details);
+            i = i + 1;
+            if (i > 2) {
+              i = 0;
+            }
+          } else {
+            console.error(conferenceResponse);
+          }
+        }
+        // Set the state to the new list of three lists of
+        // conferences
+        setConferenceColumns(columns);
+      }
+    } catch (e) {
+      console.error(e);
     }
   }
-
   useEffect(() => {
-    getConferences();
-  }, [])
-
+    fetchData();
+  }, []);
   return (
     <>
       <div className="px-4 py-5 my-5 mt-0 text-center bg-info">
@@ -70,10 +94,10 @@ function MainPage() {
       </div>
       <div className="container">
         <h2>Upcoming conferences</h2>
-        <div className="conferenceList">
-          {conferences.map(({conference}) => {
+        <div className="row">
+          {conferenceColumns.map((conferenceList, index) => {
             return (
-              <Conference key={conference.href} conference={conference} />
+              <ConferenceColumn key={index} list={conferenceList} />
             );
           })}
         </div>
@@ -81,5 +105,4 @@ function MainPage() {
     </>
   );
 }
-
 export default MainPage;
